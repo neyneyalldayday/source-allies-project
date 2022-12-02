e698aae5bd33e13065b9456efbb254f8a1c21e4e
neyneyalldayday <drfrankenstein123@gmail.com>
2022-12-02 16:54:55 -0600

    commiting code that doesnt work yet, need to map out an object to hold the top 100 words and how many times they occur but i ran out of time for the day

--- assets/index.js
+++ assets/index.js
@@ -1,21 +1,40 @@
+const clickContainer = document.querySelector(".top-words-wrapper")
+const buttonEl = document.querySelector('#find-out')
+const wordList = document.querySelector('#list')
+
 const filteredWords = []
 
 let mobyArray = mobyDick.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").split(' ')
 let stopWordsArray = stopWords.toLowerCase().split(' ')
 
+function filterStopwords() {      
+    const newMoby = mobyArray.filter(word => !stopWordsArray.includes(word))   
+    filteredWords.push(newMoby);
+    topOneHundred()
+
+}
 
 
+function topOneHundred() { 
+    
+  let mobyMap = {}
+  
 
-function filterStopwords() {  
-const newMoby = mobyArray.filter(word => !stopWordsArray.includes(word))
+    filteredWords.forEach((word) => {
+        mobyMap[word] = (mobyMap[word] || 0) + 1;   
+       
+    })
+
    
- filteredWords.push(newMoby);
+   
+
 
 }
 
+
+
+function handleClick() {    
 filterStopwords()
-
-function topOneHundred() {
-  //get the top 100
 }
-topOneHundred()
+
+clickContainer.addEventListener("click", handleClick)
